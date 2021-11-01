2c1182f48a2b7ee3d1f75342c1b99a9d0b2a0ae2
sarvadetejal <tejal.sarvade@lntinfotech.com>
2021-11-01 09:53:29 +0530

    second

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,10 +7,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Writeline("hii");
-            Console.ReadLine();
+            Console.WriteLine("hii");
             Console.WriteLine("from vs");
-
+            Console.ReadLine();
         }
     }
 }
